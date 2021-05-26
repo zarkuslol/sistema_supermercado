fff57d684c39582730f266746bfcf7e8df4d3b82
zarkus <83253722+zarkuslol@users.noreply.github.com>
2021-05-26 17:34:25 -0300

    Add files via upload

---
+++ sistema_supermercado.py
@@ -0,0 +1,48 @@
+# Sistema para supermercado/empório/mercearia e afins
+# importações do programa
+import os
+
+# variáveis para rodar o programa corretamente, NÃO MEXER
+contador = 0
+soma = 0
+titulo = 'EMPÓRIO LEAL'
+preco2 = 0
+# NÃO MEXA NESSA PARTE DE CIMA, PELO AMOR DE DEUS
+
+# programa
+while contador == 0:
+    print('-=-' * 20)
+    print(titulo.center(50))
+    print('-=-' * 20)
+    print('')
+    qtd = int(input('Unidades a serem compradas: '))
+    dup = input('Existem produtos repetidos ou mais? [1] Sim ou [2] Não: ')
+
+    # para caso houver produtos iguais na compra
+
+    if dup == '1':
+        print('')
+        b = int(input('Quantos grupos de produtos repetidos existem? '))
+        for c in range(1, b+1, 1):
+            qtd2 = int(input('Número de produtos do grupo {}: '.format(c)))
+            val = float(input('Preço unitário de um produto: R$'))
+            preco2 = qtd2 * val
+            print('Valor do grupo: R${:.2f}'.format(preco2))
+            print('')
+        for a in range(1, qtd+1, 1):
+            preco = float(input('Valor da unidade {}: R$'.format(a)))
+            soma += preco
+        print('\nTotal da compra: R${:.2f}'.format(soma))
+        input('Pressione ENTER para repetir ou saia do terminal')
+        os.system('cls')
+
+    # caso não tenha, o programa roda completo
+
+    else:
+        for a in range(1, qtd+1, 1):
+            preco = float(input('Valor da unidade {}: R$'.format(a)))
+            soma += preco
+        print('\nTotal da compra: R${:.2f}'.format(soma))
+        input('Pressione ENTER para repetir ou saia do terminal')
+        os.system('cls')
+# fim do programa
